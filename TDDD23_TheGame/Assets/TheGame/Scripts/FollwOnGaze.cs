adf9404d596308782a35e24fc1d12eaf8376bbd2
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-09-05 13:40:32 +0200

    added lantern to the game

---
+++ TDDD23_TheGame/Assets/TheGame/Scripts/FollwOnGaze.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+[RequireComponent(typeof(GazeAware))]
+public class FollwOnGaze : MonoBehaviour
+{
+    private GazeAware _gazeAware;
+	private GameObject mainCharector;
+	private bool wasSelected = false;
+    private float force = 800f;
+	// Use this for initialization
+	void Start () {
+		_gazeAware = GetComponent<GazeAware>();
+		mainCharector = GameObject.Find("FirstPersonCharacter");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_gazeAware.HasGazeFocus)
+        {
+            hover();
+        }
+        
+    }
+
+    private void hover(){
+        Vector3 side = Vector3.Cross(mainCharector.transform.forward, Vector3.up);
+        //transform.LookAt(mainCharector.transform.position);
+        Vector3 pos = mainCharector.transform.position + 2* mainCharector.transform.forward+ new Vector3(0,0.5f,0) - 0.8f*side;
+        GetComponent<Rigidbody>().AddForce((pos - transform.position) * force * Time.deltaTime);
+    }
+}
