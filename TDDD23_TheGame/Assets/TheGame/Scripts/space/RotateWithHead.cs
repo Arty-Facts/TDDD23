8a1261f047301d6c8c69ced83efbb3d62ba81d01
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-09-07 06:10:47 +0200

    Added space scean

---
+++ TDDD23_TheGame/Assets/TheGame/Scripts/space/RotateWithHead.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class RotateWithHead : MonoBehaviour
+{
+	// Use this for initialization
+	private float speed = 0.002f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HeadPose headPose = TobiiAPI.GetHeadPose();
+		if (headPose.IsRecent())
+        {
+			transform.rotation = Quaternion.Lerp(transform.rotation, headPose.Rotation, Time.time * speed);
+        }
+	}
+}
