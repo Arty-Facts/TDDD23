42988718bc6327776770f52ebdc201be8adfa246
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-09-17 16:31:16 +0200

    Added higligher

---
+++ TDDD23_TheGame/Assets/TheGame/Scripts/Movment/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public bool ClockWise = true;
+    public float Speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ClockWise){
+            transform.RotateAround(transform.position , transform.forward , Speed * Time.deltaTime);
+        }else{
+            transform.RotateAround(transform.position , -transform.forward , Speed * Time.deltaTime);
+        }
+    }
+}
--- TDDD23_TheGame/Assets/TheGame/Scripts/WeaponsController/TurretController.cs
+++ TDDD23_TheGame/Assets/TheGame/Scripts/WeaponsController/TurretController.cs
@@ -6,13 +6,15 @@ using Tobii.Gaming;
 public class TurretController : BaseController
 {
     public GameObject Turret;
+    public GameObject Charector;
     public GameObject ammo;  
+    public GameObject Higlighter; 
     private List<GameObject> Turrets = new List<GameObject>();
     private Queue<Transform> SelectQue = new Queue<Transform>();
 
     private int capacity = 300;
     private float force = 500;
-    private float speed = 3f;
+    private float speed = 6f;
     private int counter = 0;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,8 @@ public class TurretController : BaseController
         Turrets.Add(Instantiate(Turret,  new Vector3(0.8f, 0.35f, 1.7f), Quaternion.identity));
         Turrets.Add(Instantiate(Turret,  new Vector3(0f, 0.25f, 4.1f), Quaternion.identity));
         Turrets.Add(Instantiate(Turret,  new Vector3(-0.8f, 0.35f, 1.7f), Quaternion.identity));
+        Higlighter = Instantiate(Higlighter,  new Vector3(0f, 0f, -10f), Quaternion.identity);
+        
     }
 
     // Update is called once per frame
@@ -62,6 +66,10 @@ public class TurretController : BaseController
             gun.transform.rotation = new Quaternion(gun.transform.rotation.x, turret.transform.rotation.y, 0, 1);
 
         }
+        Vector3 dir = selected.position - Charector.transform.position;
+        Higlighter.transform.position = Vector3.Lerp(Higlighter.transform.position, Charector.transform.position + 5f* Vector3.Normalize(dir),  speed * Time.deltaTime);
+        Higlighter.transform.LookAt(Charector.transform.position);
+
     }
     override public void Select(Transform other){
         if(!TobiiAPI.IsConnected){
